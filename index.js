41ab005a4684a42e150a4b91eea4db0353419ee5
jwy5140 <jwy5140@gmail.com>
2019-05-02 02:34:46 -0400

    last2

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Polygon{
+    constructor(lengths){
+        this.lengths = lengths.sort();
+    }
+    get getCount(){
+        return this.lengths.length;
+    }
+    get perimeter(){
+        return this.lengths.reduce((sum, n) => {return sum+n})
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if (this.getCount === 3 && this.lengths[2] < (this.lengths[0] + this.lengths[1])){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if (this.lengths.forEach((l) => {l === this.lengths[0]})){
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
